e335ff102052309e9f03ea46d89e0e99ed73e386
Nathan Rathbun <naterathbun@gmail.com>
2017-03-02 22:47:30 -0500

    Added FrontBack

---
+++ CodingBatCSharp/Warmup1/FrontBack.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup1
+{
+    public static class FrontBack
+    {
+        public static string name = "FrontBack";
+        public static string description = "Given a string, return a new string where the first and last chars have been exchanged.";
+
+        public static string ShowAnswer(string str)
+        {
+            if (str.Length == 0 || str.Length == 1)
+            {
+                return str;
+            }
+
+            string string1 = str.Substring(0, 1);
+            string string2 = str.Substring(str.Length - 1, 1);
+
+            if (str.Length == 2)
+            {
+                return string2 + string1;
+            }
+            
+            string string3 = str.Substring(1, str.Length - 2);
+
+            return string2 + string3 + string1;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup1Tests/FrontBackTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup1;
+
+namespace CodingBatCSharpTest.Warmup1Tests
+{
+    [TestClass]
+    public class FrontBackTest
+    {
+        [TestMethod]
+        public void FrontBackTest_str_code_return_eodc()
+        {
+            Assert.AreEqual("eodc", FrontBack.ShowAnswer("code"));
+        }
+        [TestMethod]
+        public void FrontBackTest_str_a_return_a()
+        {
+            Assert.AreEqual("a", FrontBack.ShowAnswer("a"));
+        }
+        [TestMethod]
+        public void FrontBackTest_str_ab_return_ba()
+        {
+            Assert.AreEqual("ba", FrontBack.ShowAnswer("ab"));
+        }
+
+
+    }
+}
