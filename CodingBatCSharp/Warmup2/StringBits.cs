17f8f790f3a27b967c9bdaca385cb2a80949d18d
Nathan Rathbun <naterathbun@gmail.com>
2017-03-14 07:52:11 -0400

    added stringBits

---
+++ CodingBatCSharp/Warmup2/StringBits.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class StringBits
+    {
+        public string Name = "StringBits";
+        public string Description = "Given a string, return a new string made of every other char starting with the first, so 'Hello' yields 'Hlo'.";
+
+        public static string ShowAnswer(string str)
+        {
+            string returnString = string.Empty;
+
+            for (int i = 0; i < str.Length; i+=2)
+            {
+                returnString += str[i];
+            }
+
+            return returnString;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/StringBitsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class StringBitsTest
+    {
+        [TestMethod]
+        public void StringBitsTest_Hello_result_Hlo()
+        {
+            Assert.AreEqual("Hlo", StringBits.ShowAnswer("Hello"));
+        }
+        [TestMethod]
+        public void StringBitsTest_Hi_result_H()
+        {
+            Assert.AreEqual("H", StringBits.ShowAnswer("Hi"));
+        }
+        [TestMethod]
+        public void StringBitsTest_Hello_result_Heeololeo()
+        {
+            Assert.AreEqual("Hello", StringBits.ShowAnswer("Heeololeo"));
+        }
+    }
+}
