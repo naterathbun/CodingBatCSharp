a14340e5bf42f462e2d4eb454e0c253400614ebd
Nathan Rathbun <naterathbun@gmail.com>
2017-03-15 07:48:50 -0400

    Added stringsplosion

---
+++ CodingBatCSharp/Warmup2/StringSplosion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class StringSplosion
+    {
+        public string Name = "StringSplosion";
+        public string Description = "Given a non-empty string like 'Code' return a string like 'CCoCodCode'.";
+
+        public static string ShowAnswer(string str)
+        {
+            string returnString = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                returnString += str.Substring(0, i + 1);
+            }
+
+            return returnString;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/StringSplosionTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class StringSplosionTest
+    {
+        [TestMethod]
+        public void StringSplosionTest_Code_Result_CCoCodCode()
+        {
+            Assert.AreEqual("CCoCodCode", StringSplosion.ShowAnswer("Code"));
+        }
+        [TestMethod]
+        public void StringSplosionTest_abc_Result_aababc()
+        {
+            Assert.AreEqual("aababc", StringSplosion.ShowAnswer("abc"));
+        }
+        [TestMethod]
+        public void StringSplosionTest_ab_Result_aab()
+        {
+            Assert.AreEqual("aab", StringSplosion.ShowAnswer("ab"));
+        }
+    }
+}
