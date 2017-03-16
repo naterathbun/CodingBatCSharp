193e3c1c819dde49d9bc53abf2833f2d2607b8b9
Nathan Rathbun <naterathbun@gmail.com>
2017-03-16 08:11:04 -0400

    Added Last2 exercise

---
+++ CodingBatCSharp/Warmup2/Last2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class Last2
+    {
+        public string Name = "Last2";
+        public string Description = "Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so 'hixxxhi' yields 1 (we won't count the end substring).";
+
+        public static int ShowAnswer(string str)
+        {
+            string lastTwo = str.Substring(str.Length - 2, 2);
+            int count = 0;
+
+            for (int i = 0; i < str.Length-2; i++)
+            {
+                if (str[i].ToString() + str[i+1].ToString() == lastTwo)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/Last2Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class Last2Test
+    {
+        [TestMethod]
+        public void Last2Test_hixxhi_result_1()
+        {
+            Assert.AreEqual(1, Last2.ShowAnswer("hixxhi"));
+        }
+        [TestMethod]
+        public void Last2Test_xaxxaxaxx_result_1()
+        {
+            Assert.AreEqual(1, Last2.ShowAnswer("xaxxaxaxx"));
+        }
+        [TestMethod]
+        public void Last2Test_axxxaaxx_result_2()
+        {
+            Assert.AreEqual(2, Last2.ShowAnswer("axxxaaxx"));
+        }
+    }
+}
