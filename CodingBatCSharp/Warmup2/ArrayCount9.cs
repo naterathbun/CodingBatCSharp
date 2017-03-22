b737be709d315a9af204fc50f2bd34db8139e12b
Nathan Rathbun <naterathbun@gmail.com>
2017-03-22 08:08:03 -0400

    added Array9Count

---
+++ CodingBatCSharp/Warmup2/ArrayCount9.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class ArrayCount9
+    {
+        public string Name = "ArrayCount9";
+        public string Description = "Given an array of ints, return the number of 9's in the array.";
+
+        public static int ShowAnswer(int[] array1)
+        {
+            int count = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                if(array1[i] == 9)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/ArrayCount9Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class ArrayCount9Test
+    {
+        [TestMethod]
+        public void ArrayCount9Test_129_result_1()
+        {
+            Assert.AreEqual(1,ArrayCount9.ShowAnswer(new int[] { 1, 2, 9 }));
+        }
+        [TestMethod]
+        public void ArrayCount9Test_199_result_2()
+        {
+            Assert.AreEqual(2, ArrayCount9.ShowAnswer(new int[] { 1, 9, 9 }));
+        }
+        [TestMethod]
+        public void ArrayCount9Test_125_result_0()
+        {
+            Assert.AreEqual(0, ArrayCount9.ShowAnswer(new int[] { 1, 2, 5 }));
+        }
+    }
+}
