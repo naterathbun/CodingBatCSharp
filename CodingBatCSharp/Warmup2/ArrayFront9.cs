7fec9af454e6e3d514b10d61d7793264fed42589
Nathan Rathbun <naterathbun@gmail.com>
2017-03-23 08:04:04 -0400

    Added FrontArray9

---
+++ CodingBatCSharp/Warmup2/ArrayFront9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class ArrayFront9
+    {
+        public string Name = "ArrayFront9";
+        public string Description = "Given an array of ints, return true if one of the first 4 elements in the array is a 9. The array length may be less than 4.";
+
+        public static bool ShowAnswer(int[] myArray)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if(i < myArray.Length && myArray[i] == 9)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/ArrayFront9Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class ArrayFront9Test
+    {
+        [TestMethod]
+        public void ArrayFront9_12934_result_true()
+        {
+            Assert.AreEqual(true, ArrayFront9.ShowAnswer(new int[] { 1, 2, 9, 3, 4 }));
+        }
+        [TestMethod]
+        public void ArrayFront9_12349_result_false()
+        {
+            Assert.AreEqual(false, ArrayFront9.ShowAnswer(new int[] { 1, 2, 3, 4, 9 }));
+        }
+        [TestMethod]
+        public void ArrayFront9_12345_result_false()
+        {
+            Assert.AreEqual(false, ArrayFront9.ShowAnswer(new int[] { 1, 2, 3, 4, 5}));
+        }
+    }
+}
