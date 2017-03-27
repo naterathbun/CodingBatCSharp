8bf726b24b8064188db70e7ff61cf5888746d389
Nathan Rathbun <naterathbun@gmail.com>
2017-03-27 05:00:23 -0400

    Added Array123

---
+++ CodingBatCSharp/Warmup2/Array123.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class Array123
+    {
+        public string Name = "Array123";
+        public string Description = "Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.";
+
+        public static bool ShowAnswer(int[] myArray)
+        {
+            if (myArray.Length < 3)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < myArray.Length - 2; i++)
+                {
+                    if (myArray[i] == 1 && myArray[i + 1] == 2 && myArray[i + 2] == 3)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/Array123TEst.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class Array123Test
+    {
+        [TestMethod]
+        public void Array123Test_11231_true()
+        {
+            Assert.AreEqual(true, Array123.ShowAnswer(new int[] { 1, 1, 2, 3, 1 }));
+        }
+        [TestMethod]
+        public void Array123Test_11241_false()
+        {
+            Assert.AreEqual(false, Array123.ShowAnswer(new int[] { 1, 1, 2, 4, 1 }));
+        }
+        [TestMethod]
+        public void Array123Test_112123_true()
+        {
+            Assert.AreEqual(true, Array123.ShowAnswer(new int[] { 1, 1, 2, 1, 2, 3 }));
+        }
+    }
+}
