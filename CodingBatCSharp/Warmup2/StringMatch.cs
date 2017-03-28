b889a5429c70adbe305e6556b594c99d9f1a765f
Nathan Rathbun <naterathbun@gmail.com>
2017-03-28 08:23:44 -0400

    Added String Match

---
+++ CodingBatCSharp/Warmup2/StringMatch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class StringMatch
+    {
+        public string Name = "StringMatch";
+        public string Description = "Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So 'xxcaazz' and 'xxbaaz' yields 3, since the 'xx', 'aa', and 'az' substrings appear in the same place in both strings.";
+
+        public static int ShowAnswer(string string1, string string2)
+        {
+            int matchCounter = 0;
+            int shortestStringLength = Math.Min(string1.Length, string2.Length);
+
+            for (int i = 0; i < shortestStringLength - 1; i++)
+            {
+                if (string1[i] == string2[i] && string1[i+1] == string2[i+1])
+                {
+                    matchCounter++;
+                }
+            }
+
+            return matchCounter;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/StringMatchTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class StringMatchTest
+    {
+        [TestMethod]
+        public void StringMatchTest_xxcaazz_xxbaaz_result_3()
+        {
+            Assert.AreEqual(3, StringMatch.ShowAnswer("xxcaazz", "xxbaaz"));
+        }
+        [TestMethod]
+        public void StringMatchTest_abc_abc_result_2()
+        {
+            Assert.AreEqual(2, StringMatch.ShowAnswer("abc", "abc"));
+        }
+        [TestMethod]
+        public void StringMatchTest_abc_axc_result_0()
+        {
+            Assert.AreEqual(0, StringMatch.ShowAnswer("abc", "axc"));
+        }
+    }
+}
