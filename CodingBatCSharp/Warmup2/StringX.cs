f0371d421178be28d5a0c6fc4374928b36632fc3
Nathan Rathbun <naterathbun@gmail.com>
2017-03-30 07:22:13 -0400

    Added StringX

---
+++ CodingBatCSharp/Warmup2/StringX.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class StringX
+    {
+        public string Name = "StringX";
+        public string Description = "Given a string, return a version where all the 'x' have been removed. Except an 'x' at the very start or end should not be removed.";
+
+        public static string ShowAnswer(string string1)
+        {
+            string returnString = string.Empty;
+            returnString += string1[0];
+
+            for (int i = 1; i < string1.Length - 1; i++)
+            {
+                if (string1[i] != 'x')
+                {
+                    returnString += string1[i];
+                }
+            }
+            returnString += string1[string1.Length - 1];
+
+            return returnString;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/StringXTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class StringXTest
+    {
+        [TestMethod]
+        public void StringXTest_xxHxix_result_xHix()
+        {
+            Assert.AreEqual("xHix", StringX.ShowAnswer("xxHxix"));
+        }
+        [TestMethod]
+        public void StringXTest_abxxxcd_result_abcd()
+        {
+            Assert.AreEqual("abcd", StringX.ShowAnswer("abxxxcd"));
+        }
+        [TestMethod]
+        public void StringXTest_xabxxxcdx_result_xabcdx()
+        {
+            Assert.AreEqual("xabcdx", StringX.ShowAnswer("xabxxxcdx"));
+        }
+    }
+}
