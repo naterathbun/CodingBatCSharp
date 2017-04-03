3e19eb8f9f138a4226b137f8d99676be1e5835d6
Nathan Rathbun <naterathbun@gmail.com>
2017-04-03 08:41:10 -0400

    Added Alt Pairs

---
+++ CodingBatCSharp/Warmup2/AltPairs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class AltPairs
+    {
+        public string Name = "AltPairs";
+        public string Description = "Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so 'kittens' yields 'kien'.";
+
+        public static string ShowAnswer(string string1)
+        {
+            string returnString = string.Empty;
+
+            for (int i = 0; i < string1.Length; i++)
+            {
+                returnString += string1[i].ToString();
+                if (i < string1.Length - 1)
+                {
+                    returnString += string1[i + 1].ToString();
+                }
+
+                i = i + 3;
+            }
+
+            return returnString;
+        }
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/AltPairsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class AltPairsTest
+    {
+        [TestMethod]
+        public void AltPairsTest_kitten_result_kien()
+        {
+            Assert.AreEqual("kien", AltPairs.ShowAnswer("kitten"));
+        }
+        [TestMethod]
+        public void AltPairsTest_Chocolate_result_Chole()
+        {
+            Assert.AreEqual("Chole", AltPairs.ShowAnswer("Chocolate"));
+        }
+        [TestMethod]
+        public void AltPairsTest_CodingHorror_result_Congrr()
+        {
+            Assert.AreEqual("Congrr", AltPairs.ShowAnswer("CodingHorror"));
+        }
+    }
+}
