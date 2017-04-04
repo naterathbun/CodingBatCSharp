a65a0df7dd547a5d2318dec74bd93020538b880f
Nathan Rathbun <naterathbun@gmail.com>
2017-04-04 09:00:55 -0400

    Added StringYak

---
+++ CodingBatCSharp/Warmup2/StringYak.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingBatCSharp.Warmup2
+{
+    public class StringYak
+    {
+        public string Name = "StringYak";
+        public string Description = "Suppose the string 'yak' is unlucky.Given a string, return a version where all the 'yak' are removed, but the 'a' can be any char. The 'yak' strings will not overlap.";
+
+        public static string ShowAnswer(string string1)
+        {
+            string returnString = string.Empty;
+
+            for (int i = 0; i < string1.Length; i++)
+            {
+                if (i + 2 < string1.Length && string1[i] == 'y' && string1[i+2] == 'k')
+                {
+                    i = i + 2;
+                }
+                else
+                {
+                    returnString += string1[i].ToString();
+                }
+            }
+
+            return returnString;
+        }
+
+    }
+}
---
+++ CodingBatCSharpTest/Warmup2Tests/StringYakTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingBatCSharp.Warmup2;
+
+namespace CodingBatCSharpTest.Warmup2Tests
+{
+    [TestClass]
+    public class StringYakTest
+    {
+        [TestMethod]
+        public void StringYakTest_yakpak_result_pak()
+        {
+            Assert.AreEqual("pak", StringYak.ShowAnswer("yakpak"));
+        }
+        [TestMethod]
+        public void StringYakTest_pakyak_result_pak()
+        {
+            Assert.AreEqual("pak", StringYak.ShowAnswer("pakyak"));
+        }
+        [TestMethod]
+        public void StringYakTest_yak123ya_result_123ya()
+        {
+            Assert.AreEqual("123ya", StringYak.ShowAnswer("yak123ya"));
+        }
+    }
+}
